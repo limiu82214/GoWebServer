2e6f544eb5830d9ca10042d9d9f90f656bf412f4
limiu82214 <limiu82214@gmail.com>
2022-09-07 20:49:00 +0800

    version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GoWebServer
 簡易的WebServer框架
+
+
+### version
+顯示基本的html，輸入網址會尋找對應的html並顯示
+http://localhost/a.html
+http://localhost/b.html
---
+++ version/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handle)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path[len("/"):]
+	t, err := template.ParseFiles("version/" + file)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+}
